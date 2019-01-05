d230c3813b407c8a60add0fbfbf2c4f31d774e2b
Dannih95 <danielpinho95@gmail.com>
2019-01-05 09:42:36 +0000

    added button to scroll back to the top

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topButton").style.display = "block";
+  } else {
+    document.getElementById("topButton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 function hoverNavButton(elem) {
 	elem.style.backgroundColor = "white";
 	elem.childNodes[1].style.color = "black";
