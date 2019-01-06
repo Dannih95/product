d82ac187fafc26bcbf667c5a6ab481a0ee57f18d
Dannih95 <danielpinho95@gmail.com>
2019-01-06 13:44:24 +0000

    integrated youtube api with the tutorial video to pause video on exit of the tutorial popup; missing esc button working (again)

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('iframe-video', {
+	    events: {
+	    }
+	});
+}
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -43,6 +57,7 @@ function viewTutorial() {
 }
 
 function exitTutorial() {
+	player.pauseVideo();
 	document.getElementById("tutorial-video").style.visibility = "hidden";
 	var sections = document.getElementsByClassName("opacity");
 	for(let el = 0; el < sections.length; el++) {
